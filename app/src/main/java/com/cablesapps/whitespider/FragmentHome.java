58bdddf6e121c5c079e301867fdf1b0d91fad73c
georgiev84 <cska30>
2019-04-01 23:23:28 +0300

    First Upload 3

--- app/src/main/java/com/cablesapps/whitespider/FragmentHome.java
+++ app/src/main/java/com/cablesapps/whitespider/FragmentHome.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 import android.view.LayoutInflater;
@@ -14,10 +15,17 @@ import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.Toast;
 
+import com.firebase.ui.firestore.FirestoreRecyclerOptions;
+import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.CollectionReference;
 import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.Query;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
 import com.google.firebase.storage.FileDownloadTask;
 import com.google.firebase.storage.FirebaseStorage;
 import com.google.firebase.storage.StorageReference;
@@ -32,11 +40,15 @@ import static android.support.constraint.Constraints.TAG;
 
 public class FragmentHome  extends Fragment {
 
-    FirebaseFirestore db = FirebaseFirestore.getInstance();
-    FirebaseStorage storage = FirebaseStorage.getInstance();
-    String beiko;
+    private FirebaseFirestore db = FirebaseFirestore.getInstance();
+    private CollectionReference newsRef = db.collection("News");
+    private NoteAdapter adapter;
+//    FirebaseStorage storage = FirebaseStorage.getInstance();
+//    String beiko;
+//
+//    ImageView imageFront;
 
-    ImageView imageFront;
+
 
 
     @Nullable
@@ -44,54 +56,98 @@ public class FragmentHome  extends Fragment {
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_home, container, false);
 
+        setUpRecyclerView(view);
 
-        imageFront = view.findViewById(R.id.imageFront);
+//        imageFront = view.findViewById(R.id.imageFront);
+//
+//
+//        StorageReference storageRef = storage.getReference();
+//
+//
+//        // Create a new user with a first and last name
+//        Map<String, Object> news = new HashMap<>();
+//        news.put("picture", "1");
+//        news.put("title", "Концерт");
+//        news.put("content", "Начало 21:00h. Нова група алабала... нещо си");
+//
+//
+//        // Add a new document with a generated ID
+//        db.collection("news")
+//                .add(news)
+//                .addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
+//                    @Override
+//                    public void onSuccess(DocumentReference documentReference) {
+//                        Log.d(TAG, "DocumentSnapshot added with ID: " + documentReference.getId());
+//                    }
+//                })
+//                .addOnFailureListener(new OnFailureListener() {
+//                    @Override
+//                    public void onFailure(@NonNull Exception e) {
+//                        Log.w(TAG, "Error adding document", e);
+//                    }
+//                });
 
 
-        StorageReference storageRef = storage.getReference();
+//        storageRef.child("beiko.jpg").getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
+//            @Override
+//            public void onSuccess(Uri uri) {
+//                // Got the download URL for 'users/me/profile.png'
+//                Log.d(TAG, "SuccessLoadingBeiko");
+//
+//               // beiko = "https://firebasestorage.googleapis.com/v0/b/whitespider-c2711.appspot.com/o/beiko.jpg?alt=media&token=5c69a686-f932-4d56-b84e-9647c622a8b4";
+//                Picasso.with(getContext()).load(uri).fit().centerCrop().into(imageFront);
+//            }
+//        }).addOnFailureListener(new OnFailureListener() {
+//            @Override
+//            public void onFailure(@NonNull Exception exception) {
+//                // Handle any errors
+//                Log.d(TAG, "ERRORLoadingBeiko");
+//            }
+//        });
 
 
-        // Create a new user with a first and last name
-        Map<String, Object> user = new HashMap<>();
-        user.put("first", "Ada");
-        user.put("last", "Lovelace");
-        user.put("born", 1815);
-
-        // Add a new document with a generated ID
-        db.collection("users")
-                .add(user)
-                .addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
-                    @Override
-                    public void onSuccess(DocumentReference documentReference) {
-                        Log.d(TAG, "DocumentSnapshot added with ID: " + documentReference.getId());
-                    }
-                })
-                .addOnFailureListener(new OnFailureListener() {
-                    @Override
-                    public void onFailure(@NonNull Exception e) {
-                        Log.w(TAG, "Error adding document", e);
-                    }
-                });
-
-
-        storageRef.child("beiko.jpg").getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
-            @Override
-            public void onSuccess(Uri uri) {
-                // Got the download URL for 'users/me/profile.png'
-                Log.d(TAG, "SuccessLoadingBeiko");
-
-               // beiko = "https://firebasestorage.googleapis.com/v0/b/whitespider-c2711.appspot.com/o/beiko.jpg?alt=media&token=5c69a686-f932-4d56-b84e-9647c622a8b4";
-                Picasso.with(getContext()).load(uri).fit().centerCrop().into(imageFront);
-            }
-        }).addOnFailureListener(new OnFailureListener() {
-            @Override
-            public void onFailure(@NonNull Exception exception) {
-                // Handle any errors
-                Log.d(TAG, "ERRORLoadingBeiko");
-            }
-        });
-
+//        db.collection("news")
+//                .get()
+//                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+//                    @Override
+//                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
+//                        if (task.isSuccessful()) {
+//                            for (QueryDocumentSnapshot document : task.getResult()) {
+//                                Log.d(TAG, document.getId() + " => " + document.getData());
+//                            }
+//                        } else {
+//                            Log.d(TAG, "Error getting documents: ", task.getException());
+//                        }
+//                    }
+//                });
 
         return view;
+    }
+
+    private void setUpRecyclerView(View v){
+        Query query = newsRef.orderBy("priority", Query.Direction.DESCENDING);
+
+        FirestoreRecyclerOptions<Note> options = new FirestoreRecyclerOptions.Builder<Note>()
+                .setQuery(query, Note.class)
+                .build();
+
+        adapter = new NoteAdapter(options);
+
+        RecyclerView recyclerView = v.findViewById(R.id.recycler_view);
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
+        recyclerView.setAdapter(adapter);
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        adapter.startListening();
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        adapter.stopListening();
     }
 }
